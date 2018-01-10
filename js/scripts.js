661c9085686613ceb39960b3eb23586b7d3f6548
Manuela Garcia and Nanette Girzi <manuela@>
2018-01-10 11:17:22 -0800

    add street, addres, city, state, zip code to the form

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,35 @@
 $(document).ready(function() {
   $("#form").submit(function(event) {
-
     var personInput = $("input#person").val();
+    var streetInput = $("input#street").val();
+    var cityInput = $("input#city").val();
+    var stateInput = $("input#state").val();
+    var zipInput = $("input#zip").val();
+
+
 
     $(".person").text(personInput);
+    $(".street").text(streetInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zip").text(zipInput);
+
+
 
 
 
     $("#story").show();
     event.preventDefault();
   });
+
+
+
+
+
+
+
+
+
+
 
 });
